8a7928553ab908869f89dec108be8b8706d3eb41
RodolfosmFreitas <142793854+RodolfosmFreitas@users.noreply.github.com>
2023-10-16 17:44:28 +0100

    Add files via upload

--- Descriptors/descriptor_utils.py
+++ Descriptors/descriptor_utils.py
@@ -6,10 +6,11 @@ Created on Thu Aug 17 15:32:07 2023
 """
 
 from rdkit import Chem
-from mordred import Calculator, descriptors, Autocorrelation
+import numpy as np
+from mordred import Calculator, descriptors, Autocorrelation, MoeType
 
 
-def get_descriptors(smi='C', ignore_3D=True, name=None):
+class get_descriptors():#(smi='C', ignore_3D=True, name=None):
     '''
     
 
@@ -26,45 +27,57 @@ def get_descriptors(smi='C', ignore_3D=True, name=None):
     -------
     result_dict : Dict
         dictionary of molecular descriptors.
+    
+    keys : List
+        List of molecular descriptors computed.
 
     '''
-    if name is not None:
-        print('Compute the molecular descriptors of {}'.format(name))
+    def __init__(self, smi='C', 
+                 ignore_3D=True, 
+                 name=None):
+        super(get_descriptors, self).__init__()
+        self.name = name
+        self.ignore_3D = ignore_3D
+        self.smi = smi
+        
+        if self.name is not None:
+            print('Compute the molecular descriptors of {}'.format(name))
     
-    # create descriptor calculator with all descriptors
-    molecular_D = Calculator(descriptors, ignore_3D=ignore_3D) # if ignore_3D=True , no 3d descriptors are computed
+    def evaluate(self):
+        # create descriptor calculator with all descriptors
+        molecular_D = Calculator(descriptors, ignore_3D=self.ignore_3D) # if ignore_3D=True , no 3d descriptors are computed
     
-    print('Number of Molecular descriptors', len(molecular_D.descriptors))
-    mol = Chem.MolFromSmiles(smi)
+        print('Number of Molecular descriptors', len(molecular_D.descriptors))
+        mol = Chem.MolFromSmiles(self.smi)
 
-    # calculate descriptor value
-    results = molecular_D(mol)
+        # calculate descriptor value
+        results = molecular_D(mol)
     
-    # calculate descriptor value
-    results = molecular_D(mol)
+        # List of all molecular descripotrs computed
+        keys = list(results.asdict().keys())
 
-    # Delete missing values 
-    results = results.drop_missing()
+        # Delete missing values 
+        results = results.drop_missing()
 
-    # Create a dictionary
-    result_dict = results.asdict()
+        # Create a dictionary
+        result_dict = results.asdict()
 
-    # Delete zero values and boolean descriptors 
-    result_dict = {x:y for x, y in result_dict.items() if ( y!=0 and y is not (True or False or None))}
+        # Delete zero values and boolean descriptors 
+        result_dict = {x:y for x, y in result_dict.items() if ( np.abs(y) > 1e-6 and y is not (True or False or None))}
     
-    print('Number of Molecular descriptors after removing non-numerical values and zeros',len(result_dict))
+        print('Number of Molecular descriptors after removing non-numerical values and zeros',len(result_dict))
     
-    # Delete Autocorrelation descriptors: they may not directly
-    # correspond to structural or physical properties of the molecule
-    # Hollas B. An analysis of the autocorrelation descriptor for molecules. J Math
-    # Chem 2003;33(2):91–101. http://dx.doi.org/10.1023/a:1023247831238
-    Auto = Calculator(Autocorrelation)
-    Auto_dict = Auto(mol).asdict()
-    auto_list = list(Auto_dict.keys())
+        # Delete Autocorrelation descriptors: they may not directly
+        # correspond to structural or physical properties of the molecule
+        # Hollas B. An analysis of the autocorrelation descriptor for molecules. J Math
+        # Chem 2003;33(2):91–101. http://dx.doi.org/10.1023/a:1023247831238
+        Auto = Calculator(Autocorrelation)
+        Auto_dict = Auto(mol).asdict()
+        auto_list = list(Auto_dict.keys())
     
-    for k in auto_list:
-        result_dict = {x:y for x, y in result_dict.items() if x!=k}
-    
-    print('Number of Molecular descriptors after removing autocorrelation descriptors', len(result_dict))
-    
-    return result_dict
+        for k in auto_list:
+            result_dict = {x:y for x, y in result_dict.items() if x!=k}
+        
+        print('Number of Molecular descriptors after removing autocorrelation descriptors', len(result_dict))
+        
+        return result_dict , keys
